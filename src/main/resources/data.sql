2f537327a60eff28d55f55d167bf676e8c5a17b6
‘LeeJeongHun1’ <wjdgns1155@naver.com>
2023-07-25 22:33:22 +0900

    [Fix] sql 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,33 @@
+INSERT INTO `order`.product
+    (seq, name, price, stock, review_count, create_at)
+VALUES (1, '스타벅스 텀블러', 21000.0, 45, 0, '2023-07-22 13:41:06.000');
+INSERT INTO `order`.product
+    (seq, name, price, stock, review_count, create_at)
+VALUES (2, '디오디너리 데일리 세트(Daily set)', 19000.0, 89, 0, '2023-07-22 13:41:14.000');
+INSERT INTO `order`.product
+    (seq, name, price, stock, review_count, create_at)
+VALUES (3, '버드와이저 굿즈 세트', 35000.0, 43, 0, '2023-07-22 13:41:23.000');
+INSERT INTO `order`.product
+    (seq, name, price, stock, review_count, create_at)
+VALUES (4, '패브릭 포스터', 24900.0, 89, 0, '2023-07-22 13:41:30.000');
+INSERT INTO `order`.product
+    (seq, name, price, stock, review_count, create_at)
+VALUES (5, '손 세정제 대용량 500ml', 7000.0, 79, 0, '2023-07-22 13:41:36.000');
+INSERT INTO `order`.product
+    (seq, name, price, stock, review_count, create_at)
+VALUES (6, '남성 수영복 반바지', 260800.0, 26, 0, '2023-07-22 13:41:41.000');
+INSERT INTO `order`.product
+    (seq, name, price, stock, review_count, create_at)
+VALUES (7, '여행용 캐리어 24인치', 38000.0, 81, 0, '2023-07-22 13:41:44.000');
+INSERT INTO `order`.product
+    (seq, name, price, stock, review_count, create_at)
+VALUES (8, '순금 24K 목걸이', 380000.0, 85, 0, '2023-07-22 13:41:47.000');
+INSERT INTO `order`.product
+    (seq, name, price, stock, review_count, create_at)
+VALUES (9, '피케 티셔츠', 33250.0, 99, 0, '2023-07-22 13:41:52.000');
+INSERT INTO `order`.product
+    (seq, name, price, stock, review_count, create_at)
+VALUES (10, '우드테이블', 24900.0, 60, 0, '2023-07-22 13:41:56.000');
+INSERT INTO `order`.product
+    (seq, name, price, stock, review_count, create_at)
+VALUES (11, '포켓커피 초콜릿', 1980.0, 71, 0, '2023-07-22 13:41:58.000');
